e704e7fd449e2caf6a9add07f0726b98f9ac4d57
NishantWankhade <nishant.wankhade07@gmail.com>
2024-08-20 16:51:23 +0530

    Obstacle Avoidance with static change in linear and angular velocities

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,40 +2,50 @@
 
 import rospy
 from std_msgs.msg import String
+import math
 
 # Importing the LaserScan module
 from sensor_msgs.msg import LaserScan
 
+# Importing the datatype required for publishing to the CMD_VEL topic
+from geometry_msgs.msg import Twist
+
+# TurtleBot --> Burger's Dimensions
+WIDTH = 178
+RADIUS = 105
+
 def admissible(range):
-    if range > 1.5 or str(range) == "inf":
-        print(str(range))
-        return True
-    else: return False 
+    ## TODO --> Logic for the code
 
-def talker(pub, rate):
-    while not rospy.is_shutdown():
+    # Check objects in Vicinity
+    angle = int(math.degrees(math.asin((1.0 * WIDTH/2) / RADIUS)))
+    range_list = range[-angle:] + range[0:angle]
 
-        ## TODO --> Logic for the code
+    for i in range_list:
+        if i != "inf" and i < 0.2: return False
+    return True 
 
-        pub.publish(hello_str)
-        rate.sleep()
+def talker(pub, range):
+    vel = Twist()
+    if admissible(range) == True: 
+        vel.linear.x = 0.15
+        vel.angular.z = 0
+    else:
+        vel.angular.z = 0.2
+        vel.linear.x = 0
+    pub.publish(vel)
 
 def callback(data, args):
-    at_front = data.ranges[0]
-
+    range = data.ranges
     publisher_node = args[0]
-    sleep_rate = args[1]
-
-    while(admissible(at_front)):
-        talker(publisher_node, sleep_rate)
+    talker(publisher_node, range)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
 
-    rate = rospy.Rate(10) # 10hz
-    publisher = rospy.Publisher('cmd_vel', String, queue_size=10)
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    rospy.Subscriber('scan', LaserScan, callback, [publisher, rate])
+    rospy.Subscriber('scan', LaserScan, callback, [publisher])
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
